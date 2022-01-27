760046caa9534be7dee7dfa59f2296c45a80c930
Patrick Hagège <hagege.patrick@gmail.com>
2022-01-27 07:07:14 +0100

    init iteration 5

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Search = ({ searchedString, callbackSearch }) => {
     return (
         <>
+            <label htmlFor="search">Search a food in the list</label>
             <input
                 type="text"
-                placeholder="Search sa food"
+                name="search"
+                placeholder="Search a food"
                 className="input"
                 value={searchedString}
                 onChange={(e) => callbackSearch(e.target.value)}
